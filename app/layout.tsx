7bbc62573e578e7344a60743b79003e5e8f33d4d
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-01-24 14:24:19 +0100

    slight change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import SideNav from "@/components/shared/SideNav";
 import { ThemeProvider } from "@/components/shared/ThemeProvider";
 
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
