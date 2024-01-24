ec43c72bb9877b79f67bb9fc45f938012e0b177e
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-01-24 01:39:34 +0100

    .

--- components/shared/Mobilenav.tsx
+++ components/shared/Mobilenav.tsx
@@ -58,7 +58,7 @@ const Mobilenav = () => {
                         </Link>
                         <Link href='/'>
                             <Image
-                                src='/icons/icon.svg'
+                                src='/icons/Icon.svg'
                                 width={40}
                                 height={40}
                                 alt='logo'
